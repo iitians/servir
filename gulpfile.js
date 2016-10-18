ca8080686df9fa7acea5e2d8f0e01e2b8c18d957
Yefri Oscar <yefrioscar9814@hotmail.com>
2016-10-18 12:14:36 -0500

    Agregado el Login

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass',function(){
+	gulp.src('./src/scss/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: true
+	}))
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.stream());
+});
+
+
+
+gulp.task('default',function(){
+	browserSync.init({
+			server: "./"
+	});
+});
+
+gulp.watch('./src/scss/**/*.scss', ['sass']);
+gulp.watch('./*.html').on('change', browserSync.reload);
+gulp.watch('./src/scripts.js').on('change', browserSync.reload);
+gulp.watch('./src/scss/**/*.scss', browserSync.reload);
